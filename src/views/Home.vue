85831a75afb49619e2c62ada5e52b838dc2058bb
GabrielGmz <alonsogg937@gmail.com>
2025-09-27 21:19:50 -0600

    Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import Modal from '../components/modal.vue'
 import Card from '@/components/Card.vue'
+import Alert from '@/components/Alert.vue'
 
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
@@ -68,59 +69,113 @@ const products_flowers = ref([
     price: '$60.00',
     image: '/Productos/ramo6.jpg',
   },
-])
-
-const custom_arragements = ref([
   {
-    name: 'Arreglo 1',
-    description: 'Descripción del arreglo 1',
-    price: '$15.00',
-    image: '/Productos/arreglo1.jpg',
+    name: 'Ramo Amarillo',
+    description: 'Descripción del ramo amarillo',
+    price: '$70.00',
+    image: '/Productos/ramo7.jpg',
   },
   {
-    name: 'Arreglo 2',
-    description: 'Descripción del arreglo 2',
-    price: '$25.00',
-    image: '/Productos/arreglo2.jpg',
+    name: 'Ramo Rojo',
+    description: 'Descripción del ramo rojo',
+    price: '$80.00',
+    image: '/Productos/ramo8.jpg',
   },
   {
-    name: 'Arreglo 3',
-    description: 'Descripción del arreglo 3',
-    price: '$35.00',
-    image: '/Productos/arreglo3.jpg',
+    name: 'Ramo Morado',
+    description: 'Descripción del ramo morado',
+    price: '$90.00',
+    image: '/Productos/ramo9.jpg',
   },
   {
-    name: 'Arreglo 4',
-    description: 'Descripción del arreglo 4',
-    price: '$45.00',
-    image: '/Productos/arreglo4.jpg',
-  },
-])
-
-const hotwheels = ref([
-  {
-    name: 'Hotwheel 1',
-    description: 'Descripción del hotwheel 1',
-    price: '$5.00',
-    image: '/Productos/hotwheel1.jpg',
+    name: 'Ramo Verde',
+    description: 'Descripción del ramo verde',
+    price: '$100.00',
+    image: '/Productos/ramo10.jpg',
   },
   {
-    name: 'Hotwheel 2',
-    description: 'Descripción del hotwheel 2',
-    price: '$10.00',
-    image: '/Productos/hotwheel2.jpg',
+    name: 'Ramo Celeste',
+    description: 'Descripción del ramo celeste',
+    price: '$110.00',
+    image: '/Productos/ramo11.jpg',
   },
   {
-    name: 'Hotwheel 3',
-    description: 'Descripción del hotwheel 3',
-    price: '$15.00',
-    image: '/Productos/hotwheel3.jpg',
+    name: 'Ramo Fucsia',
+    description: 'Descripción del ramo fucsia',
+    price: '$120.00',
+    image: '/Productos/ramo12.jpg',
   },
   {
-    name: 'Hotwheel 4',
-    description: 'Descripción del hotwheel 4',
-    price: '$20.00',
-    image: '/Productos/hotwheel4.jpg',
+    name: 'Ramo Lavanda',
+    description: 'Descripción del ramo lavanda',
+    price: '$130.00',
+    image: '/Productos/ramo13.jpg',
+  },
+  {
+    name: 'Ramo Turquesa',
+    description: 'Descripción del ramo turquesa',
+    price: '$140.00',
+    image: '/Productos/ramo14.jpg',
+  },
+  {
+    name: 'Ramo Coral',
+    description: 'Descripción del ramo coral',
+    price: '$150.00',
+    image: '/Productos/ramo15.jpg',
+  },
+  {
+    name: 'Ramo Champagne',
+    description: 'Descripción del ramo champagne',
+    price: '$160.00',
+    image: '/Productos/ramo16.jpg',
+  },
+  {
+    name: 'Ramo Violeta',
+    description: 'Descripción del ramo violeta',
+    price: '$170.00',
+    image: '/Productos/ramo17.jpg',
+  },
+  {
+    name: 'Ramo Magenta',
+    description: 'Descripción del ramo magenta',
+    price: '$180.00',
+    image: '/Productos/ramo18.jpg',
+  },
+  {
+    name: 'Ramo Salmón',
+    description: 'Descripción del ramo salmón',
+    price: '$190.00',
+    image: '/Productos/ramo19.jpg',
+  },
+  {
+    name: 'Ramo Lavanda',
+    description: 'Descripción del ramo lavanda',
+    price: '$200.00',
+    image: '/Productos/ramo20.jpg',
+  },
+  {
+    name: 'Ramo Pistacho',
+    description: 'Descripción del ramo pistacho',
+    price: '$210.00',
+    image: '/Productos/ramo21.jpg',
+  },
+  {
+    name: 'Ramo Champagne',
+    description: 'Descripción del ramo champagne',
+    price: '$220.00',
+    image: '/Productos/ramo22.jpg',
+  },
+  {
+    name: 'Ramo Cobre',
+    description: 'Descripción del ramo cobre',
+    price: '$230.00',
+    image: '/Productos/ramo23.jpg',
+  },
+  {
+    name: 'Ramo Marfil',
+    description: 'Descripción del ramo marfil',
+    price: '$240.00',
+    image: '/Productos/ramo24.jpg',
   },
 ])
 </script>
@@ -140,27 +195,8 @@ const hotwheels = ref([
           @order-product="goToWhatsApp"
         />
       </div>
-      <h2>Arreglos Personalizados</h2>
-      <div class="product-list" v-if="true">
-        <Card
-          v-for="(product, index) in custom_arragements"
-          :key="index"
-          :product="product"
-          @click-image="openImageModal"
-          @order-product="goToWhatsApp"
-        />
-      </div>
-      <h2>Hotwheels</h2>
-      <div class="product-list" v-if="true">
-        <Card
-          v-for="(product, index) in hotwheels"
-          :key="index"
-          :product="product"
-          @click-image="openImageModal"
-          @order-product="goToWhatsApp"
-        />
-      </div>
     </main>
+    <div class="soon">Próximamente <span>más productos</span></div>
     <Modal :show="showModal" @close="closeImageModal">
       <img :src="selectedImage" alt="Imagen seleccionada" />
     </Modal>
@@ -223,6 +259,19 @@ main > h2 {
   max-width: 100%;
   max-height: 80vh;
   border-radius: 8px;
+}
+
+.soon {
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+  padding: 30px;
+  color: #ff76ba;
+  border: #ff33da 2px dashed;
+  border-radius: 8px;
+  width: fit-content;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 @media (max-width: 600px) {
